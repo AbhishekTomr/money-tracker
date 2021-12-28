beda1da28c18a2d6c033c9f17e1f102581b860b8
Abhishek Tomar <imabhishek111@gmail.com>
2021-12-28 18:29:57 +0530

    adding form validation

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -12,31 +12,55 @@ function ExpenseForm(props) {
 
 
 function onNameChangeHandler(event) {
+    if((event.target.value).trim().length)
+    {
+        event.target.classList.remove("invalid");
+    }
     setName(event.target.value);
 }
 
 function onCostChangeHandler(event) {
+    if((event.target.value).trim().length)
+    {
+        event.target.classList.remove("invalid");
+    }
     setCost(event.target.value);
 }
 
 function onDateChangeHandler(event) {
+    if((event.target.value).trim().length)
+    {
+        event.target.classList.remove("invalid");
+    }
     setDate(event.target.value);
 }
 
 function onSubmitHandler(event){
     event.preventDefault();
-    let newEntry = {
-        id : Math.random() * 100000,
-        names : name,
-        costs : cost,
-        dates : new Date(date)
-    } 
-
-    props.addExpense(newEntry);
-    setName('');
-    setCost('');
-    setDate('');
-    changeStatus();
+    if(name.trim().length===0){
+        document.querySelector(".name-feild>input").classList.add('invalid')
+    }
+    if(cost.trim().length===0){
+        document.querySelector(".cost-feild>input").classList.add('invalid')
+    }
+    if(date.trim().length===0){
+        document.querySelector(".date-feild>input").classList.add('invalid')
+    }
+    if(name.trim()&&cost.trim().length&&date.trim().length)
+    {
+        let newEntry = {
+            id : Math.random() * 100000,
+            names : name,
+            costs : cost,
+            dates : new Date(date)
+        } 
+    
+        props.addExpense(newEntry);
+        setName('');
+        setCost('');
+        setDate('');
+        changeStatus();
+    }
     
 }
 
@@ -61,15 +85,15 @@ if(formState===false){
 
 return (
     <form className="expense-form" onSubmit={onSubmitHandler}>
-    <div className="expense-feild">
+    <div className="expense-feild name-feild">
     <label>Expense Name :</label>
-    <input type='text' onChange={onNameChangeHandler} value={name} />
+    <input type='text' onChange={onNameChangeHandler}  value={name} />
     </div>
-    <div className="expense-feild">
+    <div className="expense-feild cost-feild">
     <label>Expense Cost :</label>
     <input type='number' step='0.5' min='0' onChange={onCostChangeHandler} value={cost} />
     </div>
-    <div className="expense-feild">
+    <div className="expense-feild date-feild">
     <label>Expense Date :</label>
     <input type='date' onChange={onDateChangeHandler} value={date}/>
     </div>
